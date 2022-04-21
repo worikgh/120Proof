790261555b60c2c7a295bd29a4109efb4e240d6b
Pi <5387413+worikgh@users.noreply.github.com>
2022-04-21 23:00:00 +1200

    Started implementation of Dispatcher

--- RustStuff/lpx_controll/src/main.rs
+++ RustStuff/lpx_controll/src/main.rs
@@ -1,4 +1,5 @@
 extern crate midir;
+use std::collections::HashMap;
 
 // Use the MIDI control keys from the LPX to run programes.  
 
@@ -8,28 +9,63 @@ use std::io::{stdin};
 
 use midir::{Ignore, MidiInput, MidiInputPort};
 
+// Dispatcher matches a control key to an executable and executes it
+struct Dispatcher {
+    // Associate a control value with a command
+    table: HashMap<u8, String>,    
+}
+
+impl Dispatcher {
+    fn new() -> Self {
+	let mut table:HashMap<u8, String> = HashMap::new();
+	table.insert(19, "CTL.19".to_string());
+	table.insert(29, "CTL.29".to_string());
+	table.insert(39, "CTL.39".to_string());
+	table.insert(49, "CTL.49".to_string());
+	table.insert(59, "CTL.59".to_string());
+	table.insert(69, "CTL.69".to_string());
+	table.insert(79, "CTL.79".to_string());
+	table.insert(89, "CTL.89".to_string());
+	Self {
+	    table:table,
+	}
+    }
+    fn run(&self, ctl:u8){
+	match self.table.get(&ctl) {
+	    Some(cmd) => println!("Run: {}", &cmd),
+	    None => (),
+	}
+    }
+}
+
+
 fn main() {
-    match run() {
+    let dispatcher = Dispatcher::new();
+    match run(dispatcher) {
         Ok(_) => (),
         Err(err) => println!("Error: {}", err),
     }
 }
 
-fn process_message(message:&[u8;3]){
+fn process_message(message:&[u8;3], dispatcher: &Dispatcher){
     if message[0] == 176 {
 	// A ctl message
 	
 	let key = message[1];
+	let vel = message[2];
 	if key >= 19 {
 	    // There is some noise coming from the LPX with ctl-key 7
 	    // The rest are control signals tyhat we want
-	    println!("{:?}", message);
+	    if vel > 0 {
+		// 0 VEL is pad release
+		dispatcher.run(key);
+	    }
 	}
     }
 }
 	    
 
-fn run() -> Result<(), Box<dyn Error>> {
+fn run(dispatcher:Dispatcher) -> Result<(), Box<dyn Error>> {
     let mut input = String::new();
 
     let mut midi_in = MidiInput::new("120 Proof")?;
@@ -66,7 +102,7 @@ fn run() -> Result<(), Box<dyn Error>> {
     let _conn_in = midi_in.connect(
         &in_port,
         "midir-read-input",
-        move |_stamp, message, _| {
+        move |_stamp, message, dispatcher| {
             // println!(
             //     "{}: Msg: {:?} (len = {})",
             //     (stamp as f64) / 1_000_000.0,
@@ -75,10 +111,10 @@ fn run() -> Result<(), Box<dyn Error>> {
             // );
 	    if message.len() == 3 {
 		let array = <[u8; 3]>::try_from(message).unwrap();
-		process_message(&array);
+		process_message(&array, dispatcher);
 	    }
         },
-        (),
+        dispatcher,
     )?;
 
     input.clear();
