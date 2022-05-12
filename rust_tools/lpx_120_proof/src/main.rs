2a419e8a588586a1698167e8b930685963294423
Pi <5387413+worikgh@users.noreply.github.com>
2022-05-12 19:28:42 +1200

    Set up for LPX centred on middle C

--- rust_tools/lpx_120_proof/src/main.rs
+++ rust_tools/lpx_120_proof/src/main.rs
@@ -1,7 +1,9 @@
 use midi_connection::MIDICommunicator;
+use std::env;
 use std::io::stdin;
 
 //use std::env;
+use midir;
 use std::error::Error;
 
 struct Adapter {
@@ -30,13 +32,15 @@ impl Adapter {
             0, 26, 27, 28, 29, 30, 31, 32, 33, 0, 0, 31, 32, 33, 34, 35, 36, 37, 38, 0, 0, 36, 37,
             38, 39, 40, 41, 42, 43, 0, 0,
         ];
-
+        // The middle key in this scheeme is 34.  Middle C is MIDI 60
+        // So adjustment...
+        let adj_mid_c = 60 - 34;
         let mut i = 11;
         for d in delta {
             if i > 0 {
-                let pad = d + p;
+                let pad = d + p + adj_mid_c;
                 // Incomming MIDI `i` becomes `pad`.  E.g. MIDI == 32
-                print!("pad({}) i({}) ", pad, i);
+                // print!("pad({}) i({}) ", pad, i);
                 midi_map[i] = pad;
             }
             i += 1;
@@ -49,9 +53,35 @@ impl Adapter {
     }
 }
 
+fn find_port<T>(midi_io: &T, device: &str) -> Option<T::Port>
+where
+    T: midir::MidiIO,
+{
+    let mut device_port: Option<T::Port> = None;
+    for port in midi_io.ports() {
+        if let Ok(port_name) = midi_io.port_name(&port) {
+            println!("Port: {}", &port_name);
+            if port_name.contains(device) {
+                device_port = Some(port);
+                break;
+            }
+        }
+    }
+    device_port
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
-    let midi_out: MIDICommunicator<()> =
-        MIDICommunicator::new("", "120-Proof", |_, _, _| {}, (), 2)?;
+    // let midi_out = midir::MidiOutput::new("120-Proof")?;
+    let args: Vec<_> = env::args().collect();
+    let midi_out: MIDICommunicator<()> = MIDICommunicator::new(
+        "Pure Data:Pure Data Midi-In 1",
+        "120-Proof",
+        |_, _, _| {},
+        (),
+        2,
+    )?;
+
+    let midi_input = midir::MidiInput::new("MIDITest").unwrap();
 
     let _midi_in: MIDICommunicator<Adapter> = MIDICommunicator::new(
         "Launchpad X:Launchpad X MIDI 2",
