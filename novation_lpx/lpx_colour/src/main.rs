4659c28b84dc36d23febf945e7bf5a5f62e8f936
Pi <5387413+worikgh@users.noreply.github.com>
2022-05-25 19:56:01 +1200

    Working now

--- novation_lpx/lpx_colour/src/main.rs
+++ novation_lpx/lpx_colour/src/main.rs
@@ -2,34 +2,29 @@ use midi_connection::MIDICommunicator;
 
 use std::env;
 use std::error::Error;
-fn get_colour(args: &Vec<String>) -> Result<(u8, u8, u8), Box<dyn Error>> {
-    let red: u8 = args[1].parse()?;
-    let green: u8 = args[2].parse()?;
-    let blue: u8 = args[3].parse()?;
-    Ok((red, green, blue))
-}
+// use std::io::stdin;
 fn main() -> Result<(), Box<dyn Error>> {
     // Get the pad (11..99) and colour (r,g,b)
     let args: Vec<String> = env::args().collect();
-    match args[1].parse::<u8>() {
-        Err(err) => eprintln!("{:?}", err),
-        Ok(pad) => {
-            match get_colour(&args) {
-                Err(err) => eprintln!("{:?}", err),
-                Ok((red, green, blue)) => {
-                    let mut midi_communicator1 = MIDICommunicator::new(
-                        "Launchpad X:Launchpad X MIDI 1",
-                        "120-Proof-1",
-                        |_, _, _| {},
-                        (),
-                        1,
-                    )?;
-                    let msg: [u8; 13] = [240, 0, 32, 41, 2, 12, 3, 3, pad, red, green, blue, 247];
-                    // let msg: [u8; 12] = [240, 0, 32, 41, 2, 12, 3, pad, red, green, blue, 247];
-                    midi_communicator1.send(&msg).unwrap()
-                }
-            };
-        }
-    };
+
+    let mut midi_communicator1 = MIDICommunicator::new(
+        "Launchpad X:Launchpad X MIDI 1",
+        "120-Proof-1",
+        |_, _, _| {},
+        (),
+        2,
+    )?;
+
+    let pad: u8 = args[1].parse()?;
+    let red: u8 = args[2].parse()?;
+    let green: u8 = args[3].parse()?;
+    let blue: u8 = args[4].parse()?;
+    let msg: [u8; 13] = [240, 0, 32, 41, 2, 12, 3, 3, pad, red, green, blue, 247];
+
+    midi_communicator1.send(&msg)?;
+
+    // let mut input: String = String::new();
+    // input.clear();
+    // stdin().read_line(&mut input)?; // wait for next enter key press
     Ok(())
 }
