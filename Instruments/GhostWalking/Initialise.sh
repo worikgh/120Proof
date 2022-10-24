9960f7660f9e1c72aab7ecf3dbe643363d9caad9
Pi <5387413+worikgh@users.noreply.github.com>
2022-10-24 18:21:22 +1300

    Switch to One20Proof.pm

--- Instruments/GhostWalking/Initialise.sh
+++ Instruments/GhostWalking/Initialise.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/perl -w
 use strict;
+use lib("$ENV{'Home120Proof'}/Perl");
+use One20Proof;
 
 my $time = scalar(localtime());
 print "Start GhostWalking $time\n";
 
-use lib("$ENV{'Home120Proof'}/Perl");
-use One20Proof;
+## Kill these if they exist.  They would conflict with what is run here
+&One20Proof::pkill('lpx_manager');
+&One20Proof::pkill('yoshimi');
+
 
 # Must have jack
 `jack_wait -w`;
@@ -13,27 +17,36 @@ if(!$0){
     die "Failed waiting jack: $0\n";
 }
 
+## lpx_control must be running
+if(!`pgrep lpx_control`){
+    die "lpx_control must be running";
+}
 
-## Kill these if they exist
-&One20Proof::pkill('lpx_manager');
-&One20Proof::pkill('yoshimi');
+my $jack_name = 'GhostWalkingKeys';
+my $midi_name =  'yoshimi-GhostWalkingKeys';
+
 &One20Proof::run_daemon("$ENV{'Home120Proof'}/bin/InitialiseYos  GhostWalkingKeys '$ENV{'Home120Proof'}/Instruments/xiz/Hammond Organ.xiz'");
+&One20Proof::wait_for_jack($jack_name) or die "Jack: $jack_name not found";
+&One20Proof::wait_for_midi($midi_name) or die "$midi_name not found";
+
+
+$jack_name = 'GhostWalkingLPX';
+$midi_name =  'yoshimi-GhostWalkingKeys';
+
 &One20Proof::run_daemon("$ENV{'Home120Proof'}/bin/InitialiseYos GhostWalkingLPX '$ENV{'Home120Proof'}/Instruments/xiz/Wide Bass.xiz'");
-
-while (! `jack_lsp |grep GhostWalkingLPX` ){
-    print "Waiting for jack GhostWalkingLPX\n";
-    sleep 1;
-}
-
-while (! `jack_lsp |grep GhostWalkingKeys` ){
-    print "Waiting for jack GhostWalkingKeys\n";
-    sleep 1;
-}
+&One20Proof::wait_for_jack($jack_name) or die "Jack: $jack_name not found";
+&One20Proof::wait_for_midi($midi_name) or die "$midi_name not found";
 
 &One20Proof::run_daemon("$ENV{'Home120Proof'}/bin/lpx_manager $ENV{'Home120Proof'}/Instruments/GhostWalking/lpx_manager.cfg 69 1 4 6 9 11 < /dev/null ");
 
-# TODO: How can it be determined lpx_manager is running?
-sleep 1;
+# Wait until lpx_manager is running
+&One20Proof::wait_for_midi("120-Proof-MIDI-In-LPX") or
+    die "120-Proof-MIDI-In-LPX not found";
+&One20Proof::wait_for_midi("120-Proof-MIDI-Out-LPX") or
+    die "120-Proof-MIDI-Out-LPX not found";
+&One20Proof::wait_for_midi("120-Proof-MIDI-Out-PD") or
+    die "120-Proof-MIDI-Out-PD not found";
 
-&One20Proof::run_daemon("$ENV{'Home120Proof'}/bin/InitialiseMidi $ENV{'Home120Proof'}/Instruments/GhostWalking/midi.cfg ");
+#&One20Proof::run_daemon("$ENV{'Home120Proof'}/bin/InitialiseMidi $ENV{'Home120Proof'}/Instruments/GhostWalking/midi.cfg ");
+print `$ENV{'Home120Proof'}/bin/InitialiseMidi $ENV{'Home120Proof'}/Instruments/GhostWalking/midi.cfg`;
 # TODO: Get a way of of confirming MDI set up correctly
