ae227278abd7bb18034539f01975fe83f3bf7cd1
Pi <5387413+worikgh@users.noreply.github.com>
2022-10-24 18:24:31 +1300

    Make more useful

--- Perl/One20Proof.pm
+++ Perl/One20Proof.pm
@@ -14,8 +14,55 @@ sub run_daemon( $ ) {
     my $cmd = shift or die "Must pass command";
     defined(my $pid = fork())   or die "can't fork: $!";
     return($pid) if $pid;               # non-zero now means I am the parent
-    (setsid() != -1)            or die "Can't start a new session: $!";
+    # (setsid() != -1)            or die "Can't start a new session: $!";
     `$cmd`;
 }
+
+
+## Wait for a MIDI client to come up.  After five seconds give up
+sub wait_for_midi {
+    my $midi_name = shift or die "Pass a midi name to wait for";
+    chomp $midi_name;
+
+    my $time_out = 5; # Five seconds
+    my $delay = 0.05;
+    my $loops = $time_out / $delay; ## How many loops until time out
+    my $counter = 0;
+    while(1){
+	$counter++;
+	if($counter > $loops){
+	    return 0;
+	}else{
+	    foreach my $client (grep{/^client \d+:\s+\'([^\']+)\'/} `aconnect -l`){
+		$client =~ /^client \d+:\s+\'([^\']+)\'/ or die ;
+		if($midi_name eq $1 ){
+		    return 1;
+		}
+	    }
+	}
+	select(undef, undef, undef, 0.05);
+    }
+}    
+
+sub wait_for_jack {
+    my $jack_name = shift or die "Pass a midi name to wait for";
+    chomp $jack_name;
+
+    my $time_out = 5; # Five seconds
+    my $delay = 0.05;
+    my $loops = $time_out / $delay; ## How many loops until time out
+    my $counter = 0;
+    while(1){
+	$counter++;
+	if($counter > $loops){
+	    return 0;
+	}else{
+	    if(`jack_lsp |grep GhostWalkingKeys`){
+		return 1;
+	    }
+	}
+	select(undef, undef, undef, 0.05);
+    }
+}    
 
 1;
