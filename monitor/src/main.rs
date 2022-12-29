b683021d0f4483e0f5433e96cd20bdc6f4e6c0b3
Pi <5387413+worikgh@users.noreply.github.com>
2022-12-30 11:42:20 +1300

    Working, mostly

--- monitor/src/main.rs
+++ monitor/src/main.rs
@@ -1,3 +1,5 @@
+/// Monitor the output of all the programmes started using
+/// One20Proof::run_daemon.
 use std::collections::HashMap;
 use std::env;
 use std::fs;
@@ -8,76 +10,157 @@ use std::io::SeekFrom;
 use std::path::Path;
 use std::thread;
 use std::time;
+mod default_filter;
 mod file_filter;
 mod file_record;
-// use file_filter::FileFilter;
+mod yoshimi_out_filter;
+
+use default_filter::DefaultFilter;
 use file_record::FileRecord;
+use yoshimi_out_filter::YoshimiOutFilter;
+
+const OUTPUT_DIR: &str = "output";
+
+/// Get the names of all the files in the output directory
+fn get_file_names(output_dir_path: &Path) -> Vec<String> {
+    fs::read_dir(output_dir_path)
+        .unwrap()
+        .map(|x| {
+            x.unwrap()
+                .path()
+                .into_os_string()
+                .to_str()
+                .unwrap()
+                .to_string()
+        })
+        .collect()
+}
+
+/// Check to see if a file has any fresh data:
+/// `file_name` is complete path to the file
+/// `file_position` is the position in the file to read from
+/// Return the data and the new position in the file
+/// The data can be an empty string and the new position the old position.
+/// If the file is new but empty the data will be an empty string and
+/// the new position zero
+fn refresh_file(file_name: String, file_position: u64) -> io::Result<(String, u64)> {
+    let mut f = fs::File::open(&file_name)?;
+    let fsize = f.metadata().unwrap().len();
+
+    if fsize < file_position {
+        // File has been reset.  Read from start
+        f.seek(SeekFrom::Start(0)).unwrap();
+    } else {
+        // File is in play read from position
+        f.seek(SeekFrom::Start(file_position)).unwrap();
+    }
+    let mut buffer: Vec<u8> = Vec::new();
+    let n = f.read_to_end(&mut buffer)?;
+
+    if n > 0 {
+        // Got some data
+        Ok((String::from_utf8(buffer).unwrap(), fsize))
+    } else {
+        Ok(("".to_string(), fsize))
+    }
+}
 
 fn main() -> io::Result<()> {
+    // Main cache of data about the files being monitored
     let mut file_store: HashMap<String, FileRecord> = HashMap::new();
     let home = env::var("Home120Proof").unwrap();
-    let output_dir_path = Path::new(&home).join("output");
+    let output_dir_path = Path::new(&home).join(OUTPUT_DIR);
 
+    // Filters to use for the different files
+    let mut default_filter = DefaultFilter {};
+    let mut yoshimi_out_filter = YoshimiOutFilter::new();
     loop {
-        let files = fs::read_dir(output_dir_path.clone()).unwrap();
-        for path in files {
-            let file_name: String = format!(
-                "{}/{}",
-                output_dir_path.display(),
-                path.unwrap().file_name().as_os_str().to_str().unwrap()
-            );
+        let files = get_file_names(&output_dir_path);
 
-            if !file_store.contains_key(&file_name) {
+        for file_name in files.iter() {
+            if !file_store.contains_key(file_name) {
+                // First time this file has been seen. Initialise a
+                // data record for it.
                 file_store.insert(file_name.clone(), FileRecord::new());
             }
-            let mut f = match fs::File::open(&file_name) {
-                Ok(f) => f,
+
+            match refresh_file(
+                file_name.to_string(),
+                file_store.get(file_name).unwrap().position,
+            ) {
+                Ok((new_data, n)) => {
+                    // File still exists.
+                    file_store.entry(file_name.clone()).and_modify(|fr| {
+                        fr.cache = new_data;
+                        fr.position = n;
+                    });
+                }
                 Err(err) => {
                     // File cannot be read.  If it exists, panic.  Else
                     // delete from cache and continue
                     match err.kind() {
                         ErrorKind::NotFound => {
                             println!("Removing filename: {}", &file_name);
-                            file_store.remove(&file_name);
+                            file_store.remove(file_name);
                             continue;
                         }
                         _ => panic!("{:?}", err.kind()),
                     };
                 }
             };
-            let file_position: u64 = file_store[&file_name].position;
-            let fsize = f.metadata().unwrap().len();
-            if fsize < file_position {
-                // File has been reset.  Read from start
-                f.seek(SeekFrom::Start(0)).unwrap();
-            } else {
-                // File is in play read from position
-                f.seek(SeekFrom::Start(file_position)).unwrap();
-            }
-            let mut buffer: Vec<u8> = Vec::new();
-            let n = f.read_to_end(&mut buffer)?;
-
-            // Is this needed since we did `read_to_end`?
-            f.seek(SeekFrom::Current(n as i64)).unwrap();
-
-            if n > 0 {
-                // Got some data
-                let new_data = String::from_utf8(buffer).unwrap();
-                file_store.entry(file_name.clone()).and_modify(|fr| {
-                    fr.cache += new_data.as_str();
-                    fr.position += n as u64;
-                });
-            }
         }
 
         for (f, mut v) in &mut file_store {
-            let summary = v.summarise(None);
-            if summary.len() > 0 {
-                println!("f: {}: {}", f, summary);
-                v.cache = String::new();
+            let summary = match f.as_str() {
+                "yohimi.out" => v.summarise(&mut yoshimi_out_filter),
+                _ => v.summarise(&mut default_filter),
+            };
+            for s in summary.iter() {
+                println!("f: {}: {}", f, s.len());
             }
+            v.cache = String::new();
         }
         thread::sleep(time::Duration::from_millis(100));
     }
     // Ok(())
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    /// A bit of a convoluted test.  Cannot think of a simpler way to test
+    fn test_file_names() {
+        use std::env::temp_dir;
+        use std::fs::create_dir;
+        use std::path::Path;
+        use std::path::PathBuf;
+        use std::process;
+        let temp_dir_name = format!("test_dir_{}", process::id());
+        let mut test_path: PathBuf = temp_dir();
+        test_path.push(temp_dir_name.as_str());
+
+        let test_dir_path = test_path.as_path();
+        assert!(!Path::exists(test_dir_path));
+        create_dir(test_dir_path).unwrap();
+        assert!(Path::exists(test_dir_path));
+        assert!(Path::is_dir(test_dir_path));
+
+        let file_names = get_file_names(test_dir_path);
+        assert!(file_names.is_empty());
+
+        let temp_file_name = format!("test_file_{}", process::id());
+        let mut test_file_path = test_path.clone();
+        test_file_path.push(temp_file_name.as_str());
+        let test_file_path = test_file_path.as_path();
+        let _file = fs::File::create(test_file_path).unwrap();
+        assert!(Path::is_file(test_file_path));
+
+        let file_names = get_file_names(test_dir_path);
+        assert!(file_names.len() == 1);
+
+        // Clean up
+        fs::remove_file(test_file_path).unwrap();
+        fs::remove_dir(test_dir_path).unwrap();
+        assert!(!Path::exists(test_dir_path));
+    }
 }
