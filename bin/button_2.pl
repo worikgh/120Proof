ee5bbcf05f9b4978527fd186855cd5a10fb09214
Pi <5387413+worikgh@users.noreply.github.com>
2023-02-28 07:36:47 +1300

    Deal with boards that have no effects, just pass through

---
+++ bin/button_2.pl
@@ -0,0 +1,7 @@
+#!/usr/bin/perl -w
+use strict;
+print "------- $0 \n";
+print `sudo  --preserve-env=Home120Proof  -u patch /home/patch/120Proof/bin/Mistress  KILL` ;
+# print `systemctl restart jack.service`  ;
+print `systemctl start modep-mod-ui.service`;
+print "------- $0 $< $ENV{Home120Proof}\n";#  
