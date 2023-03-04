0ac5b88ed3843e8ceab3afbb8a84411ce9331d2e
Pi <5387413+worikgh@users.noreply.github.com>
2023-03-05 11:15:21 +1300

    Minor improvements

--- bin/button_1.pl
+++ bin/button_1.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -w
 use strict;
 print "------- $0 \n";
-print `systemctl stop modep-mod-ui.service`;
+print `systemctl stop modep-mod-host.service`;
 print `sudo -u patch --preserve-env /home/patch/120Proof/bin/Mistress`;
 print "------- $0 $< $ENV{Home120Proof}\n";#  
