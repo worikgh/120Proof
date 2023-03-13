230a0c23934a8ce82b6f2147626ab76ebe357418
Pi <5387413+worikgh@users.noreply.github.com>
2023-03-13 21:25:23 +1300

    Recognises pedals.

--- pedal/midi_driver/src/main.rs
+++ pedal/midi_driver/src/main.rs
@@ -3,7 +3,10 @@ use std::env::args;
 use std::error::Error;
 use std::io::BufRead;
 use std::io::BufReader;
+use std::thread::sleep;
+use std::time::Duration;
 use subprocess::Exec;
+use subprocess::Redirection;
 // use subprocess::Communicator;
 // use subprocess::Popen;
 // use subprocess::PopenConfig;
@@ -15,34 +18,44 @@ fn main() -> Result<(), Box<dyn Error>> {
 }
 
 fn run(name: String) -> Result<(), Box<(dyn std::error::Error + 'static)>> {
-    let x = Exec::cmd(ASEQDUMP)
-        .arg("-p")
-        .arg(name.as_str())
-        .stream_stdout()
-        .unwrap();
-    let br = BufReader::new(x);
     let re = Regex::new(r"Program change\s+\d+, program (\d+)$").unwrap();
-    for (i, line) in br.lines().enumerate() {
-        //32:0   Program change Program change          0, program 16
-        let line = match line {
-            Ok(l) => l,
-            Err(err) => panic!["Died {err}"],
-        };
-        let cap = match re.captures(line.as_str()) {
-            Some(c) => c,
-            None => {
-                println!("Did not match: {line}");
-                continue;
+    loop {
+        let _x = match Exec::cmd(ASEQDUMP)
+            .arg("-p")
+            .arg(name.as_str())
+            .stderr(Redirection::Merge)
+            .stream_stdout()
+        {
+            Ok(x) => {
+                println!("Started {ASEQDUMP} -p {name}");
+                let br = BufReader::new(x);
+                for line in br.lines() {
+                    //32:0   Program change Program change          0, program 16
+                    let line = match line {
+                        Ok(l) => {
+                            // println!(">{l}");
+                            l
+                        }
+                        Err(err) => panic!["Died {err}"],
+                    };
+                    let cap = match re.captures(line.as_str()) {
+                        Some(c) => c,
+                        None => {
+                            println!("Did not match: {line} ");
+                            continue;
+                        }
+                    };
+                    let sig = cap[1].parse::<i32>().unwrap();
+                    println!("Pedal: {sig}");
+                    continue;
+                }
+            }
+            Err(err) => {
+                eprintln!("Failed: {err}");
             }
         };
-        let sig = cap[1].parse::<i32>().unwrap();
-        println!("{}: {}", i, sig);
+        sleep(Duration::from_millis(765));
+        println!("Bottom of loop");
     }
-    // Command::new(aseqdump)
-    //     .arg(name.as_str())
-    //     .stdout(Stdio::piped())
-    //     .output()
-    //     .expect("Failed to execute command");
-    // let z: String = output.stdout.unwrap();
-    Ok(())
+    // Ok(())
 }
