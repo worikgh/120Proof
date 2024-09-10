adcca54fb325ba124e9cf1914776c3862279093d
Worik <5387413+worikgh@users.noreply.github.com>
2024-09-10 21:03:13 +1200

    Infinite loop making meters work

--- bin/metermonitor.pl
+++ bin/metermonitor.pl
@@ -22,31 +22,31 @@ if(!`pgrep -f $tuner`){
     open(my $fh, '|-', "$tuner >/dev/null 2>&1 &") or die $!;
 }
 
-my @jackc = ();
 
-foreach (&One20Proof::all_jack_connections()){
-    /\"([^\"]+)\" \"([^\"]+)\"/ or die $_;
-    push @jackc, [$1, $2];
-    my $one = $1;
-    my $two = $2;
-    if($2 =~ /system:playback_\d+/){
-	print "$one $two\n";
+sub get_jackc(){
+}	
+
+while(1){
+    my @jackc = ();
+    foreach (&One20Proof::all_jack_connections()){
+	/^.(.+)" "(.+).$/ or die $_;
+    	push @jackc, [$1,   $2];
+    }	
+
+    ## Find pipe to output
+    my @outputc =  grep{$_ !~ /^mod-monitor/}map{$_->[0]}grep{$_->[1] =~ /system:playback/} @jackc;
+    #grep{$_->[1] =~ /system:playback_\d+/ } @jackc;
+    my $out_to_m = undef;
+    foreach (@outputc){
+	$out_to_m = $_;
+	last;
     }
-}
 
-## Find pipe to output
-my @outputc =  grep{$_ !~ /^mod-monitor/}map{$_->[0]}grep{$_->[1] =~ /system:playback/} @jackc;
-#grep{$_->[1] =~ /system:playback_\d+/ } @jackc;
-my $out_to_m = undef;
-foreach (@outputc){
-    print "> $_ \n";
-    $out_to_m = $_;
-    last;
+    my $m_cmd1 = "jack_connect '$out_to_m' 'Nordic Meter (Stereo):inR'";
+    my $m_cmd2 = "jack_connect system:capture_1 'Nordic Meter (Stereo):inL'";
+    my $t_cmd =  "jack_connect system:capture_1 gxtuner:in_0";
+    print `$m_cmd1 2>/dev/null`;
+    print `$m_cmd2 2>/dev/null`;
+    print `$t_cmd  2>/dev/null`;
+    sleep 0.5;
 }
-
-my $m_cmd1 = "jack_connect '$out_to_m' 'Nordic Meter (Stereo):inR'";
-my $m_cmd2 = "jack_connect system:capture_1 'Nordic Meter (Stereo):inL'";
-my $t_cmd =  "jack_connect system:capture_1 gxtuner:in_0";
-print `$m_cmd1`;
-print `$m_cmd2`;
-print `$t_cmd`;
